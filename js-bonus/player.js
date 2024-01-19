1013b24f967cc829f11103fdbbbdd3dbc44fa756
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2024-01-19 11:58:35 +0100

    dom race game solution

---
+++ js-bonus/player.js
@@ -0,0 +1,42 @@
+class Player extends Component {
+  constructor(gameScreen, left, top, width, height, imgSrc) {
+    super(gameScreen, left, top, width, height, imgSrc);
+    this.directionX = 0;
+    this.directionY = 0;
+  }
+
+  move() {
+    this.left += this.directionX;
+    this.top += this.directionY;
+    debugger;
+    if (this.left < 10) {
+      this.left = 10;
+    }
+    if (this.top < 10) {
+      this.top = 10;
+    }
+    if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
+      this.left = this.gameScreen.offsetWidth - this.width - 10;
+    }
+    if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
+      this.top = this.gameScreen.offsetHeight - this.height - 10;
+    }
+    this.updatePosition();
+  }
+
+  didCollide(obstacle) {
+    const playerRect = this.element.getBoundingClientRect();
+    const obstacleRect = obstacle.element.getBoundingClientRect();
+    if (
+      playerRect.left < obstacleRect.right &&
+      playerRect.right > obstacleRect.left &&
+      playerRect.top < obstacleRect.bottom &&
+      playerRect.bottom > obstacleRect.top
+    ) {
+      console.log("Crash!");
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
